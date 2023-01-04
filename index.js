2b90e12d73ac0a3c197f63b0c896512675f8c4ad
Amar-04 <115002355+Amar-04@users.noreply.github.com>
2023-01-04 22:02:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var randomNum1=Math.floor(Math.random()*6)+1;
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNum1 + ".png");
+
+var randomNum2=Math.floor(Math.random()*6)+1;
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNum2 + ".png");
+
+if (randomNum1>randomNum2) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!"
+}
+else if (randomNum1<randomNum2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins🚩!"
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw!"
+}
